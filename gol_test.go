7765d0de2dfee1f71f8d23ad83ca66345bbb11fa
vijayanand500 <69767520+vijayanand500@users.noreply.github.com>
2020-08-23 18:49:34 -0400

    Create gol_test.go

---
+++ gol_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestGameOfLife(t *testing.T) {
+    var inpGOL [][]int = [][]int{{0,0,0},{1,1,1},{0,0,0}}
+    var expOutput [][]int = [][]int{{0,1,0},{0,1,0},{0,1,0}}
+    outGOL := gameOfLife(inpGOL)
+    
+    if len(expOutput) != len(outGOL) {
+       t.Errorf("Length of the output is not correct")
+    }
+    
+    for row := range(outGOL) {
+      for column := range(outGOL[0]) {
+        if outGOL[row][column] != expOutput[row][column] {
+          t.Errorf("Output of GOL is not as expected at row: %d and column %d", row, column)
+         }
+       }
+    } 
+      
+}
